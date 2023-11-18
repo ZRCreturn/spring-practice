3fc17345c5bd514988ed06d909710224d50ba70b
Ruichen Zhang <374992826@qq.com>
2023-11-18 16:51:04 -0500

    add new category

--- src/main/java/com/returnZhang/reggie/controller/CategoryController.java
+++ src/main/java/com/returnZhang/reggie/controller/CategoryController.java
@@ -1,7 +1,12 @@
 package com.returnZhang.reggie.controller;
 
+import com.returnZhang.reggie.common.R;
+import com.returnZhang.reggie.entity.Category;
 import com.returnZhang.reggie.service.CategoryService;
+import org.apache.ibatis.annotations.Mapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,4 +16,13 @@ public class CategoryController {
     @Autowired
     private CategoryService categoryService;
 
+    /**
+     * 新增分类
+     * @return
+     */
+    @PostMapping
+    public R<String> save(@RequestBody Category category){
+        categoryService.save(category);
+        return R.success("新增分类成功");
+    }
 }
