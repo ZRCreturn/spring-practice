029a7ac504d08df01085758a4b3bd20ca7c1baf1
Ruichen Zhang <374992826@qq.com>
2023-11-18 17:12:14 -0500

    add page query and delete category

--- src/main/java/com/returnZhang/reggie/controller/CategoryController.java
+++ src/main/java/com/returnZhang/reggie/controller/CategoryController.java
@@ -1,14 +1,13 @@
 package com.returnZhang.reggie.controller;
 
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.returnZhang.reggie.common.R;
 import com.returnZhang.reggie.entity.Category;
 import com.returnZhang.reggie.service.CategoryService;
 import org.apache.ibatis.annotations.Mapper;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/category")
@@ -21,8 +20,27 @@ public class CategoryController {
      * @return
      */
     @PostMapping
-    public R<String> save(@RequestBody Category category){
+    public R<String> save(@RequestBody Category category) {
         categoryService.save(category);
         return R.success("新增分类成功");
+    }
+
+    @GetMapping("/page")
+    public R<Page> page(int page, int pageSize) {
+        // 分页构造器
+        Page<Category> pageInfo = new Page<>(page, pageSize);
+        //条件构造器
+        LambdaQueryWrapper<Category> queryWrapper = new LambdaQueryWrapper<>();
+        queryWrapper.orderByAsc(Category::getSort);
+
+        categoryService.page(pageInfo, queryWrapper);
+
+        return R.success(pageInfo);
+    }
+
+    @DeleteMapping
+    public R<String> delete(Long id){
+        categoryService.removeById(id);
+        return R.success("分类信息删除成功");
     }
 }
--- src/main/java/com/returnZhang/reggie/service/CategoryService.java
+++ src/main/java/com/returnZhang/reggie/service/CategoryService.java
@@ -2,7 +2,6 @@ package com.returnZhang.reggie.service;
 
 import com.baomidou.mybatisplus.extension.service.IService;
 import com.returnZhang.reggie.entity.Category;
-import org.springframework.stereotype.Service;
 
 
 public interface CategoryService extends IService<Category> {
